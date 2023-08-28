65acba6100081cd8bb12c5ecfe1c6325e78e2380
Brendan Landis <b@slownames.net>
2023-08-28 15:42:10 -0400

    Band dropdown in News page displays correct bands

---
+++ app/(admin)/api/getOtherBandsQuery.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+const accessToken = Cookies.get('accessToken');
+import { Band } from '@/app/types';
+
+const fetchBands = async () => {
+    try {
+    const response = await axios
+        .get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/me/?populate=*`, {
+            headers: {
+                Authorization: `bearer ${accessToken}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+const transformBandsData = (data) => {
+    const transformedBandsArray: Band[] = data.otherbands.map((band) => ({
+        id: band.id,
+        bandname: band.bandname,
+        selected: false,
+    }));
+    const mainBand: Band = {
+        id: data.mainband.id,
+        bandname: data.mainband.bandname,
+        selected: true,
+    }
+    transformedBandsArray.unshift(mainBand);
+    return transformedBandsArray;
+}
+
+const getOtherBandsQuery = () => {
+    const { data, ...rest } = useQuery(['bandsDropDown'], fetchBands);
+    const transformedData = data ? transformBandsData(data) : [];
+    return  {
+        data: transformedData,
+        ...rest
+    }
+};
+
+export default getOtherBandsQuery;
--- app/(admin)/sharedcomponents/WhichBand.tsx
+++ app/(admin)/sharedcomponents/WhichBand.tsx
@@ -6,8 +6,20 @@ import Cookies from 'js-cookie';
 const accessToken = Cookies.get('accessToken');
 import { useQuery } from '@tanstack/react-query';
 
+import getOtherBandsQuery from '../api/getOtherBandsQuery';
+
 export default function WhichBand() {
-    const [userBands, setUserBands] = useState<Band[]>([]);
+    // const [userBands, setUserBands] = useState<Band[]>([]);
+
+    const { data: selectedBands, isLoading, isError } = getOtherBandsQuery();
+
+    if (isLoading) {
+        return <>loading...</>;
+    }
+
+    if (isError) {
+        return <>error fetching data</>
+    }
 
     // const { isLoading, isError, data, error } = useQuery<any[], Error>({
     //     queryKey: ['userBandsz'],
@@ -32,19 +44,19 @@ export default function WhichBand() {
     //     return <>{error.message}</>;
     // }
 
-    useEffect(() => {
-        const newBandArray: Band[] = [
-            { id: 3, bandname: 'Anura', selected: true },
-            { id: 4, bandname: 'Big Hiatus', selected: true },
-            { id: 9, bandname: 'Brendan Landis', selected: false },
-            { id: 5, bandname: 'Esther Chlorine', selected: true },
-            { id: 10, bandname: 'Star Card', selected: true },
-        ];
+    // useEffect(() => {
+    //     const newBandArray: Band[] = [
+    //         { id: 3, bandname: 'Anura', selected: true },
+    //         { id: 4, bandname: 'Big Hiatus', selected: true },
+    //         { id: 9, bandname: 'Brendan Landis', selected: false },
+    //         { id: 5, bandname: 'Esther Chlorine', selected: true },
+    //         { id: 10, bandname: 'Star Card', selected: true },
+    //     ];
 
-        const selectedBands = newBandArray.filter(band => band.selected);
+    //     const selectedBands = newBandArray.filter(band => band.selected);
 
-        setUserBands(selectedBands);
-    }, []);
+    //     setUserBands(selectedBands);
+    // }, []);
 
     return (
         <>
@@ -52,7 +64,7 @@ export default function WhichBand() {
                 what band is this for?
             </label>
             <select defaultValue="0" id="news-form-band">
-                {userBands.map((band) => (
+                {selectedBands.map((band) => (
                     <option key={band.id} value={band.id}>
                         {band.bandname}
                     </option>
