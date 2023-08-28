063a0364399ea8d60d797386f6ad46149dc2a882
Brendan Landis <b@slownames.net>
2023-08-28 09:46:18 -0400

    upgrade packages, move types to ts file, make GetHumanName smoother

--- app/(admin)/api/GetHumanName.tsx
+++ app/(admin)/api/GetHumanName.tsx
@@ -3,21 +3,18 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 const accessToken = Cookies.get('accessToken');
 import { useQuery } from '@tanstack/react-query';
-
+import { User } from '../../types';
 
 export default function GetHumanName() {
-    const { isLoading, isError, data, error } = useQuery({
+    const { isLoading, isError, data, error } = useQuery<User, Error>({
         queryKey: ['username'],
         queryFn: () =>
             axios
-                .get(
-                    `${process.env.NEXT_PUBLIC_STRAPI_URL}/users/me?populate=*`,
-                    {
-                        headers: {
-                            Authorization: `bearer ${accessToken}`,
-                        },
-                    }
-                )
+                .get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/me`, {
+                    headers: {
+                        Authorization: `bearer ${accessToken}`,
+                    },
+                })
                 .then((res) => res.data),
     });
 
@@ -25,9 +22,8 @@ export default function GetHumanName() {
         return <>loading...</>;
     }
 
-    if (error) {
-        return <>{error}</>
+    if (isError) {
+        return <>{error.message}</>;
     }
-
     return <>{data.username}</>;
 }
---
+++ app/types.ts
@@ -0,0 +1,16 @@
+export interface Band {
+    id: number;
+    bandname: string;
+    selected: boolean;
+}
+
+export interface User {
+    blocked: boolean;
+    confirmed: boolean;
+    createdAt: string;
+    email: string;
+    id: number;
+    provider: string;
+    updatedAt: string;
+    username: string;
+}
